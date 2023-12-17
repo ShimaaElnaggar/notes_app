1aeaf5f393d421bf8f302c06cdba7e3a9c95984a
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2023-12-17 19:39:12 +0200

    create ReadNoteCubit

---
+++ lib/cubits/read_note_cubit/read_note_cubit.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/read_note_cubit/read_note_states.dart';
+
+class ReadNoteCubit extends Cubit<ReadNoteStates>{
+  ReadNoteCubit(super.initialState);
+
+}

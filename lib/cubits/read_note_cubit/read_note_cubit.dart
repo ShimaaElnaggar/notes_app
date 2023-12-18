e3ca391ea7fb34d33255a702b9f7d8110ccbfcad
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2023-12-18 16:01:45 +0200

    display notes part one

--- lib/cubits/read_note_cubit/read_note_cubit.dart
+++ lib/cubits/read_note_cubit/read_note_cubit.dart
@@ -7,15 +7,11 @@ import 'package:notes_app/models/note_model.dart';
 
 class ReadNoteCubit extends Cubit<ReadNoteStates>{
   ReadNoteCubit(super.initialState);
-
-  fetchNotes( NoteModel notes){
-
-    try{
+  List<NoteModel> ? notes;
+  fetchNotes( ){
       var notesBox = Hive.box<NoteModel>(kNotesBox);
-       List<NoteModel> notes = notesBox.values.toList();
-      emit(ReadNoteSuccess(notes));
-    }catch(e){
-      emit(ReadNoteFailure(e.toString()));
-    }
+        notes = notesBox.values.toList();  //getter for data we don't need using try and catch
+
+
   }
   }
--- lib/cubits/read_note_cubit/read_note_states.dart
+++ lib/cubits/read_note_cubit/read_note_states.dart
@@ -1,16 +1,6 @@
 
-import 'package:notes_app/models/note_model.dart';
+
 
 abstract class ReadNoteStates{}
   class ReadNoteInitial extends ReadNoteStates{}
-  class ReadNoteLoading extends ReadNoteStates{}
-  class ReadNoteSuccess extends ReadNoteStates{
-  final List<NoteModel> notes;
 
-  ReadNoteSuccess(this.notes);
-}
-  class ReadNoteFailure extends ReadNoteStates{
-  final String errorMessage;
-
-  ReadNoteFailure(this.errorMessage);
-  }

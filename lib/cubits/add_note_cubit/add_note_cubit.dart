13e5ef5866519a59c4ea4a6f502eb5dea9fee900
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2023-12-19 13:36:16 +0200

    addNoteBottomSheet done

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
@@ -6,8 +8,9 @@ import 'package:notes_app/models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNoteStates>{
   AddNoteCubit(super.initialState);
-
+ Color color = const Color(0xffCDB4DB);
   addNote(NoteModel note)async{
+  note.color = color.value;
     emit(AddNoteLoading());
     try{
    var notesBox = Hive.box<NoteModel>(kNotesBox);
